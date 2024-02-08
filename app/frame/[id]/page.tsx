a30f12ac3dba47e57c54574a72e53befd872155d
nader <dabit3@gmail.com>
2024-02-08 16:15:51 -0600

    update with page rendering

--- app/frame/[id]/page.tsx
+++ app/frame/[id]/page.tsx
@@ -1,27 +1,42 @@
+import Image from 'next/image'
+
 export const runtime = 'edge'
+
+const image = "https://i.imgur.com/E5e7907.png"
+const link = 'https://nader.codes'
+const buttonText = 'Visit'
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world.</h1>
+      <Image
+        src={image}
+        width={1910}
+        height={1000}
+        alt='Hello world.'
+      />
+      <a
+      className='button'
+      href={link} target="_blank" rel="no-opener">
+        {buttonText}
+      </a>
     </div>
   );
 }
 
 export async function generateMetadata({ params }) {
-  const image = "https://i.imgur.com/E5e7907.png"
 
   const meta = {
     'fc:frame': 'vNext',
     'fc:frame:image': image,
-    'fc:frame:button:1': 'Visit',
+    'fc:frame:button:1': buttonText,
     'fc:frame:button:1:action': 'link',
-    'fc:frame:button:1:target': 'https://nader.codes',
+    'fc:frame:button:1:target': link,
     'hey:portal': 'vNext',
     'hey:portal:image': image,
-    'hey:portal:button:1': 'Visit',
+    'hey:portal:button:1': buttonText,
     'hey:portal:button:1:type': 'link',
-    'hey:portal:button:1:target': 'https://nader.codes',
+    'hey:portal:button:1:target': link
   }
 
   return {
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,45 @@
+import Image from 'next/image'
+
 export const runtime = 'edge'
+
+const image = "https://i.imgur.com/E5e7907.png"
+const link = 'https://nader.codes'
+const buttonText = 'Visit'
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world.</h1>
+      <img
+        src={image}
+        width={700}
+        height={350}
+        alt='Hello world.'
+      />
+      <a
+      href={link} target="_blank" rel="no-opener">
+        <div className='
+        md:w-[700px]
+        w-full mt-1 hover:bg-[#f1f1f1] transition rounded py-2 bg-white px-12 flex items-center w-[200px] justify-center'>
+         <p className='text-black'>{buttonText}</p>
+        </div>
+      </a>
     </div>
   );
 }
 
 export async function generateMetadata({ params }) {
-  const image = "https://i.imgur.com/E5e7907.png"
 
   const meta = {
     'fc:frame': 'vNext',
     'fc:frame:image': image,
-    'fc:frame:button:1': 'Visit',
+    'fc:frame:button:1': buttonText,
     'fc:frame:button:1:action': 'link',
-    'fc:frame:button:1:target': 'https://nader.codes',
+    'fc:frame:button:1:target': link,
     'hey:portal': 'vNext',
     'hey:portal:image': image,
-    'hey:portal:button:1': 'Visit',
+    'hey:portal:button:1': buttonText,
     'hey:portal:button:1:type': 'link',
-    'hey:portal:button:1:target': 'https://nader.codes',
+    'hey:portal:button:1:target': link
   }
 
   return {
